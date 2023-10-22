d50ad6a3f775725afa69bdee5b78473cdb1ed977
Nikita <nikita.peresunko32@gmail.com>
2023-10-22 17:38:19 +0300

    Pushed changes

---
+++ MusicBot/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbrar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
