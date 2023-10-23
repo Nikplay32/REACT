254df4787f77bce5b3f3b9052b13bfe447dedafe
Nikita <nikita.peresunko32@gmail.com>
2023-10-23 20:11:16 +0300

    Update

--- MusicBot/src/App.jsx
+++ MusicBot/src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from './components/Navbrar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import Card from './components/Card'
+import Goals from './components/Goals'
+import About from './components/About'
 import './App.css'
 
 function App() {
@@ -9,7 +11,13 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <div className='separator'></div>
+      <About />
+      <div className='separator'></div>
+      <Goals />
+      <div className='separator'></div>
       <Card />
+      <div className='separator'></div>
       <Footer />
     </div>
   )
--- MusicBot/src/components/Card.jsx
+++ MusicBot/src/components/Card.jsx
@@ -4,7 +4,7 @@ import './Card.css'
 export default function Card() {
     return (
         <div className="card">
-            <h1 className="main--title">About us</h1>
+            <h1 className="main--title">Our priorities</h1>
             <ul className="main--facts">
                 <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusantium officia.</li>
                 <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
